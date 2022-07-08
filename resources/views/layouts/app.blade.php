e652c0179b77d1c1921f01e62623a669c49ac291
soft-raph <ariyoraphael1@gmail.com>
2022-07-08 11:02:53 +0100

    added a get request to web route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,39 +16,37 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="bg-gray-900 text-white">
-    <header class="border-b border-gray-800">
+    <body class="bg-gray-900">
+    <header class="border-b border-gray-800 bg-gray-900 ">
         <nav class="container mx-auto flex items-center justify-between px-4 py-6">
             <div class="flex items-center ">
                 <a href="/">
-                    <svg class="svg-icon" viewBox="0 0 20 20">
+                    <svg class="svg-icon flex-none" width="40" height="40" viewBox="0 0 20 20">
                         <path d="M11.709,7.438H8.292c-0.234,0-0.427,0.192-0.427,0.427v8.542c0,0.234,0.192,0.427,0.427,0.427h3.417c0.233,0,0.426-0.192,0.426-0.427V7.865C12.135,7.63,11.942,7.438,11.709,7.438 M11.282,15.979H8.719V8.292h2.563V15.979zM6.156,11.709H2.74c-0.235,0-0.427,0.191-0.427,0.426v4.271c0,0.234,0.192,0.427,0.427,0.427h3.417c0.235,0,0.427-0.192,0.427-0.427v-4.271C6.583,11.9,6.391,11.709,6.156,11.709 M5.729,15.979H3.167v-3.416h2.562V15.979zM17.261,3.167h-3.417c-0.235,0-0.427,0.192-0.427,0.427v12.812c0,0.234,0.191,0.427,0.427,0.427h3.417c0.234,0,0.427-0.192,0.427-0.427V3.594C17.688,3.359,17.495,3.167,17.261,3.167 M16.833,15.979h-2.562V4.021h2.562V15.979z"></path>
                     </svg>
                 </a>
-
+                <ul class="flex ml-0 lg:ml-16 space-x-8 mt-6 lg:mt-0">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
             </div>
-
+            <div class="flex items-center mt-6 lg:mt-0">
+                <livewire:search-dropdown>
+                    <div class="ml-6">
+                        <a href="#"><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8"></a>
+                    </div>
+            </div>
         </nav>
 
     </header>
-
-
-
-
-{{--        <div class="min-h-screen bg-gray-100">--}}
-{{--            @include('layouts.navigation')--}}
-
-{{--            <!-- Page Heading -->--}}
-{{--            <header class="bg-white shadow">--}}
-{{--                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">--}}
-{{--                    {{ $header }}--}}
-{{--                </div>--}}
-{{--            </header>--}}
-
-{{--            <!-- Page Content -->--}}
-{{--            <main>--}}
-{{--                {{ $slot }}--}}
-{{--            </main>--}}
-{{--        </div>--}}
+    <main class="py-8">
+        @yield('content')
+    </main>
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="https://api.igdb.com" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
     </body>
 </html>
